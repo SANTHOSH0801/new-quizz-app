44f4596cecbd43b6a4cf5a3c9b6598e6a90c0596
SANTHOSH <126708441+SANTHOSH0801@users.noreply.github.com>
2025-03-23 01:33:23 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -24,7 +24,7 @@ console.log("Allowed Frontend URL:", process.env.FRONTEND_URL);
 
 
 app.use(cors({ 
-    origin: ["https://new-quiz-942m21vab-santhosh0801s-projects.vercel.app", "https://new-quizz-rbe1zaia6-santhosh0801s-projects.vercel.app"],
+    origin: "*",
     methods: "GET,POST,PUT,DELETE,OPTIONS",
     allowedHeaders: "Content-Type,Authorization",
     credentials: true
